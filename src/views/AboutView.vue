b14054e9f2efdfa8fd35ab4519acd7c86d92f108
wangmengjia <2986894865@qq.com>
2022-08-10 19:17:36 +0800

    first commit

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,22 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <a-button type="primary" @click="getNum"> Primary </a-button>
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {}
+  },
+  created() {},
+  mounted() {},
+  methods: {
+    getNum() {
+      const a = 10;
+      console.log("🚀 ~ file: AboutView.vue ~ line 18 ~ getNum ~ a", a)
+    }
+  }
+}
+</script>
